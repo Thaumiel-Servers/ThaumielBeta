56ce4422eeeae2478eeb14072a6439e13481297d
Mr. Baguetter <70399915+Mr-Baguetter@users.noreply.github.com>
2024-08-22 22:45:24 -0500

    Gif fade fix?

--- Javascript.js
+++ Javascript.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const blackSquare1 = document.getElementById('blackSquare1');
         const blackSquare2 = document.getElementById('blackSquare2');
 
-        const darkImage = 'dark.png';
+        const darkImage = 'Dark.png';
 
         function checkAndShowImages() {
             const currentImage = buttonImage.src.split('/').pop();
